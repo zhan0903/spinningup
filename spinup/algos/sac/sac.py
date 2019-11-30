1401b3d01a5474137517df4977df347b11465a52
zhan0903 <z.han0903@gmail.com>
2019-11-30 11:56:48 +1100

    refactor to get rid of spinup framework

--- spinup/algos/sac/sac.py
+++ spinup/algos/sac/sac.py
@@ -2,9 +2,9 @@ import numpy as np
 import tensorflow as tf
 import gym
 import time
-from spinup.algos.sac import core
-from spinup.algos.sac.core import get_vars
-from spinup.utils.logx import EpochLogger
+import core
+from core import get_vars
+from spinup.logx import EpochLogger
 
 
 class ReplayBuffer:
